95f5fcd1af6f2a30ed73bb223f485b53bece559c
Tze <jonathabejose@gmail.com>
2026-04-21 18:27:26 -0600

    Update route.ts

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,8 @@
+export const dynamic = "force-static";
+
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
 export const { GET } = createFromSource(source, {
-  // https://docs.orama.com/docs/orama-js/supported-languages
   language: 'english',
 });
