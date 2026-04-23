e6f7e36a7ca5e0c9809feced4f10231922ebedf7
Tze <jonathabejose@gmail.com>
2026-04-22 21:47:42 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,12 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
+export const metadata = {
+  icons: {
+    icon: '/shiny/favicon.ico',
+  },
+}
+
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
